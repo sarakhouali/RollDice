bf261176ed21f5443f8b4b3cb701841ef26bdc8d
Sara KHOUALI <sara.khouali@gmail.com>
2023-09-10 22:04:38 +0200

    switch the player = done

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const score1El = document.getElementById("score_1");
 
 const diceEl = document.querySelector(".dice");
 
-// const current0El = document.getElementById("current_0");
-// const current1El = document.getElementById("current_1");
+const current0El = document.getElementById("current_0");
+const current1El = document.getElementById("current_1");
+// player
+const player0El = document.querySelector(".player_0");
+const player1El = document.querySelector(".player_1");
+
 //btns
 const btnRoll = document.getElementById("btn_roll");
 
@@ -13,16 +17,37 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+// cleare score
+let currentScore = 0;
+//active player
+let activePlayer = 0;
+
+
 btnRoll.addEventListener('click', function(){
     diceEl.classList.remove("hidden");
 
 //1. generate the random number
     const dice = Math.floor(Math.random() * 6) + 1;
-//1. afficher random image
+
+//2. afficher random image
     diceEl.src = `./image/dice-${dice}.png`;
-    
 
+//3. check for rolled 1
+    if (dice != 1) {
+        //display the score
+        currentScore += dice;
+        //dynamic select of player
+        document.getElementById(`current_${activePlayer}`).textContent = currentScore;
 
+    } else {
+        //switch the player if player have number 1
+        document.getElementById(`current_${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore =0;
+        player0El.classList.toggle("player_active");
+        player1El.classList.toggle("player_active");
+
+    }
 });
 
 
@@ -32,8 +57,6 @@ btnRoll.addEventListener('click', function(){
 
 
 
-let activePlayer = 0;
-let currentScore = 0;
 
 
 
@@ -52,24 +75,5 @@ let currentScore = 0;
 
 
 
-
-//     // generate the random number
-//     const dice = Math.floor(Math.random() * 6) + 1;
-//     // afficher random image
-//     diceEl.src = `./image/dice-${dice}.png`;
-//     console.log(diceEl);
-//     //check for rolled 1
-//     if (dice != 1) {
-//         //display score
-//         currentScore += dice;
-//         document.getElementById(`current_${activePlayer}`).textContent = currentScore;
-//     } else {
-//         //switch the player
-        
-//     }
-
-
-
-// });
 
 
